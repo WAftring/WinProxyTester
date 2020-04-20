b9d2c93fbbbf2069fb16ab4c91929fc93089baa8
Will Aftring <willaftring@gmail.com>
2020-04-20 13:37:40 -0500

    First working release

---
+++ WinProxyTester/ReadMe.md
@@ -0,0 +1,15 @@
+## Introduction
+
+A simple app for testing WININET and WINHTTP configurations.
+
+## Usage
+
+```
+USAGE
+        WinProxyTester.exe [URL]
+                -d Go direct
+                -i Use WININET          -h Use WINHTTP
+                -n [proxy] Use specified proxy
+                -r Read proxy settings for current user
+                -w Write output of HTTP Query
+```
--- WinProxyTester/WinProxyTester.cpp
+++ WinProxyTester/WinProxyTester.cpp
@@ -5,20 +5,66 @@
 #include <Windows.h>
 #include <Winhttp.h>
 
-LPCWSTR g_URL;
-DWORD g_dwAccessType;
-LPCWSTR g_pszProxyW;
-LPCWSTR g_pszProxyBypassW;
-DWORD g_dwFlags;
+#define NONE 0x0
+#define READ 0x1
+#define TEST 0x2
+#define WRITE 0x4
 
-BOOL ParseCmdArgs(char* argv[]);
-BOOL ParseCmdArgs(char* argv[])
+BOOL ParseCmdArgs(char* argv[], int argc, std::wstring &URL, DWORD &dwAccessType, LPCWSTR &pszProxyW, LPCWSTR &pszProxyBypassW, DWORD &dwFlags, BYTE &AppFlags);
+BOOL ParseCmdArgs(char* argv[], int argc, std::wstring &URL, DWORD &dwAccessType, LPCWSTR& pszProxyW, LPCWSTR& pszProxyBypassW, DWORD &dwFlags, BYTE &AppFlags)
 {
+	std::string Temp = std::string(argv[1]);
+	URL = std::wstring(Temp.begin(), Temp.end());
 
+	for (size_t i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "-r") == 0)
+		{
+			AppFlags |= READ;
+		}
+		else if (strcmp(argv[i], "-d") == 0)
+		{
+			dwAccessType = WINHTTP_ACCESS_TYPE_NO_PROXY;
+			AppFlags |= TEST;
+		}
+		else if (strcmp(argv[i], "-h") == 0)
+		{
+			dwAccessType = WINHTTP_ACCESS_TYPE_DEFAULT_PROXY;
+			AppFlags |= TEST;
+		}
+		else if (strcmp(argv[i], "-i") == 0)
+		{
+			dwAccessType = WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY;
+			AppFlags |= TEST;
+		}
+		else if (strcmp(argv[i], "-n") == 0)
+		{
+			dwAccessType = WINHTTP_ACCESS_TYPE_NAMED_PROXY;
+			AppFlags |= TEST;
+			if (argc > 3)
+			{
+				pszProxyW = (LPCWSTR)argv[3];
+			}
+			else
+			{
+				printf("Please enter a proxy when using -n flag\n");
+				exit(1);
+			}
+		}
+		else if (strcmp(argv[i], "-w") == 0)
+		{
+			AppFlags |= WRITE;
+		}
+	}
+	return TRUE;
 }
 int main(int argc, char *argv[])
 {
-	
+	BYTE AppFlags = 0;
+	DWORD dwAccessType;
+	LPCWSTR pszProxyW = NULL;
+	LPCWSTR pszProxyBypassW = NULL;
+	DWORD dwFlags;
     DWORD dwSize = 0;
     DWORD dwDownloaded = 0;
     LPSTR pszOutBuffer = 0;
@@ -26,79 +72,102 @@ int main(int argc, char *argv[])
     HINTERNET 	hSession = NULL,
 				hConnect = NULL,
 				hRequest = NULL;
+	WINHTTP_CURRENT_USER_IE_PROXY_CONFIG pINETProxyConfig;
+	WINHTTP_PROXY_INFO pHTTPProxyInfo;
+	std::wstring URL;
+
 	if (argc < 2)
 	{
-		printf("Here we are printing out all of our important info...\n");
+		printf("USAGE\n");
+		printf("\tWinProxyTester.exe [URL]\n");
+		printf("\t\t-d Go direct\n");
+		printf("\t\t-i Use WININET\n");
+		printf("\t\t-h Use WINHTTP\n");
+		printf("\t\t-n [proxy] Use specified proxy\n");
+		printf("\t\t-r Read proxy settings for current user\n");
+		printf("\t\t-w Write output of HTTP Query\n");
 		exit(1);
 	}
-
-
-    hSession = WinHttpOpen(L"WinHTTP Example/1.0",
-							WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
-							WINHTTP_NO_PROXY_NAME,
-							WINHTTP_NO_PROXY_BYPASS,0);
-							
-	if(hSession)
-		hConnect = WinHttpConnect(hSession, L"www.microsoft.com",
-									INTERNET_DEFAULT_HTTPS_PORT,0);
-	if(hConnect)
-		hRequest = WinHttpOpenRequest(hConnect, L"GET", NULL,
-										NULL, WINHTTP_NO_REFERER,
-										WINHTTP_DEFAULT_ACCEPT_TYPES,
-										WINHTTP_FLAG_SECURE);
-	if(hRequest)
-		bResults = WinHttpSendRequest(hRequest,
-										WINHTTP_NO_ADDITIONAL_HEADERS, 0,
-										WINHTTP_NO_REQUEST_DATA, 0,
-										0,0);
-										
-	if(bResults)
-		bResults = WinHttpReceiveResponse(hRequest, NULL);
 	
-	if(bResults)
+	ParseCmdArgs(argv, argc, URL, dwAccessType, pszProxyW, pszProxyBypassW, dwFlags, AppFlags);
+	if ((AppFlags & READ) == READ)
 	{
-		do
+		if (WinHttpGetIEProxyConfigForCurrentUser(&pINETProxyConfig))
 		{
-			dwSize = 0;
-			if(!WinHttpQueryDataAvailable(hRequest, &dwSize))
-				printf("Error %u in WinHttpQueryDataAvailable.\n",
-				GetLastError());
-			pszOutBuffer = new char [dwSize+1];
-			if(!pszOutBuffer)
-			{
-				printf("Out of memory\n");
-				dwSize = 0;
-			}
-			else
-			{
-				ZeroMemory(pszOutBuffer, dwSize+1);
-				if(!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer,
-					dwSize, &dwDownloaded))
-					printf("Error %u in WinHttpReadData.\n", GetLastError());
-				else
-					printf("%s", pszOutBuffer);
-				delete [] pszOutBuffer;
-			}
-		} while (dwSize > 0);
+			wprintf(L"WININET Settings:\n");
+			wprintf(L"Access Type: %d, Auto-Config URL: %s, Proxy String: %s, Bypass-List: %s\n", pINETProxyConfig.fAutoDetect, pINETProxyConfig.lpszAutoConfigUrl, pINETProxyConfig.lpszProxy, pINETProxyConfig.lpszProxyBypass);
+		}
+		if (WinHttpGetDefaultProxyConfiguration(&pHTTPProxyInfo))
+		{
+			wprintf(L"WinHTTP Setting:\n");
+			wprintf(L"Access Type: %d, Proxy String: %s, Bypass-List: %s\n", pHTTPProxyInfo.dwAccessType, pHTTPProxyInfo.lpszProxy, pHTTPProxyInfo.lpszProxyBypass);
+		}
+		else
+		{
+			printf("Failed with %d\n", GetLastError());
+			exit(1);
+		}
 	}
+	if ((AppFlags & TEST) == TEST)
+	{
+		hSession = WinHttpOpen(L"WinHTTP Example/1.0",
+			dwAccessType,
+			pszProxyW,
+			pszProxyBypassW, 0);
 
-	if(!bResults)
-		printf("Error %d has occurred.\n", GetLastError());
-	
-	if(hRequest) WinHttpCloseHandle(hRequest);
-	if(hConnect) WinHttpCloseHandle(hConnect);
-	if(hSession) WinHttpCloseHandle(hSession);
+		if (hSession)
+			hConnect = WinHttpConnect(hSession, URL.c_str(),
+				INTERNET_DEFAULT_HTTPS_PORT, 0);
+		if (hConnect)
+			hRequest = WinHttpOpenRequest(hConnect, L"GET", NULL,
+				NULL, WINHTTP_NO_REFERER,
+				WINHTTP_DEFAULT_ACCEPT_TYPES,
+				WINHTTP_FLAG_SECURE);
+		if (hRequest)
+			bResults = WinHttpSendRequest(hRequest,
+				WINHTTP_NO_ADDITIONAL_HEADERS, 0,
+				WINHTTP_NO_REQUEST_DATA, 0,
+				0, 0);
+
+		if (bResults)
+			bResults = WinHttpReceiveResponse(hRequest, NULL);
+
+		if (bResults && (AppFlags & WRITE) == WRITE)
+		{
+			do
+			{
+				dwSize = 0;
+				if (!WinHttpQueryDataAvailable(hRequest, &dwSize))
+					printf("Error %u in WinHttpQueryDataAvailable.\n",
+						GetLastError());
+				pszOutBuffer = new char[dwSize + 1];
+				if (!pszOutBuffer)
+				{
+					printf("Out of memory\n");
+					dwSize = 0;
+				}
+				else
+				{
+					ZeroMemory(pszOutBuffer, dwSize + 1);
+					if (!WinHttpReadData(hRequest, (LPVOID)pszOutBuffer,
+						dwSize, &dwDownloaded))
+						printf("Error %u in WinHttpReadData.\n", GetLastError());
+					else
+						printf("%s", pszOutBuffer);
+					delete[] pszOutBuffer;
+				}
+			} while (dwSize > 0);
+		}
+
+		if (!bResults)
+			printf("Error %d has occurred.\n", GetLastError());
+		else
+			printf("Successfully reached %s\n", argv[1]); //@TODO Add a conversion for the WINERROR codes
+	}
+	if (hRequest) WinHttpCloseHandle(hRequest);
+	if (hConnect) WinHttpCloseHandle(hConnect);
+	if (hSession) WinHttpCloseHandle(hSession);
+	return 0;
 	
 	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
